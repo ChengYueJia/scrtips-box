2f080bf7f43a90875a5cbde52c0afb2d9846ab73
Paul Cheng <chengyuejia@foxmail.com>
2024-12-14 21:49:51 +0800

    feat(git): add script to update user.name & user.email in git history

---
+++ git/update_history_user_name_email.sh
@@ -0,0 +1,17 @@
+
+git filter-branch --env-filter '
+OLD_EMAIL="old_user_email"
+CORRECT_NAME="new_user_name"
+CORRECT_EMAIL="new_user_email"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' -- --all
